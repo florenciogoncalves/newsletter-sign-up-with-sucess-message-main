d54c962a34c1d7bbecfc96253b575e3734b6dc65
Florêncio Gonçalves <109610920+florencioGoncalves@users.noreply.github.com>
2023-06-22 14:02:46 +0100

    Validação de email

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,11 +4,10 @@ element.addEventListener("input", () => {
   let bol = true
 	bol = element.value.includes('@') && bol ? true : false
 	bol = element.value.includes('.') && bol ? true : false
+	bol = element.value[element.value.length - 1] != '.' && bol ? true : false
 	if (bol) {
-    console.log('Foi adicionado')
 		form.classList.remove("wrong");
 	} else {
-  console.log('Foi removido')
 		form.classList.add("wrong");
 	}
 });
